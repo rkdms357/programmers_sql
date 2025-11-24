c44fdca5a5147250476d749ea1f07d060fa4a04b
GAEUN JEONG <79949390+rkdms357@users.noreply.github.com>
2025-11-24 21:43:08 +0900

    Update 고양이와 개는 몇 마리 있을까.sql

--- 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -1,4 +1,5 @@
 select ANIMAL_TYPE, count(ANIMAL_TYPE)
 from ANIMAL_INS
 group by ANIMAL_TYPE
+--order by decode(ANIMAL_TYPE, 'Cat', 1), ANIMAL_TYPE;
 order by (case when ANIMAL_TYPE = 'Cat' then 1 else 2 end)
